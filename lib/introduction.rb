26f2c85718be3905fb6fb934aac968fa5d0076fb
restful-strategy-4685 <planetlucid@gmail.com>
2019-07-27 20:52:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,3 +8,8 @@ introduction
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+#introduction_with_language_optional
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
