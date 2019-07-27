994782dd7156ef53743253cc3f41e75d2aa5bad0
restful-strategy-4685 <planetlucid@gmail.com>
2019-07-27 20:49:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,10 @@
 #introduction
-
 def introduction(name)  
   puts "Hi, my name is #{name}."
 end
-
 introduction
 
-def greeting_programmer(name, language)
+#introduction_with_language
+def introduction_with_language(name, language)
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
